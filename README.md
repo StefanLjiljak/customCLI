7b3fd1f17bfce8fb919112d5e66a768b178bc616
StefanLjiljak <stefanljiljak@gmail.com>
2020-06-02 13:40:49 +0200

    Creating custom Command line interface (CLI) For Cryptocurrency Prices using Node.js

---
+++ README.md
@@ -0,0 +1,5 @@
+# Creating custom Command line interface (CLI) For Cryptocurrency Prices using Node.js
+
+- demo apiKey='demo-26240835858194712a4f8cc0dc635c7a';
+
+- svakog dana u svakom pogledu sve vise napredujem ;)
---
+++ commands/key.js
@@ -0,0 +1,53 @@
+const inquirer = require('inquirer');
+const colors = require('colors');
+const KeyManager = require('../lib/KeyManager');
+const { isRequired } = require('../utils/validation');
+
+const key = {
+    async set() {
+        const keyManager = new KeyManager();
+        const input = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'key',
+                message: 'Enter API Key '.green + 'https://nomics.com',
+                validate: isRequired
+            }
+        ]);
+
+        const key = keyManager.setKey(input.key);
+
+        if (key) {
+            console.log('API Key Set'.blue);
+        }
+    },
+    show() {
+        try {
+            const keyManager = new KeyManager();
+            const key = keyManager.getKey();
+
+            console.log('Current API Key: ', key.yellow);
+
+            return key;
+        } catch (err) {
+            console.error(err.message.red);
+        }
+    },
+    remove() {
+        // if there is more then one key remove method
+        // should be like remove({id})...
+        try {
+            const keyManager = new KeyManager();
+            keyManager.deleteKey();
+            // .deleteKey({id})
+
+            console.log('Key Removed'.blue);
+
+            return;
+        } catch (err) {
+            console.error(err.message.red);
+        }
+    }
+}
+
+module.exports = key;
